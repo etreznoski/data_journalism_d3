59f202e9e9b161845403eaad9c60c17461f6626b
etreznoski <sanchez.elizabethk@gmail.com>
2021-04-16 14:35:07 -0700

    set up xscale functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@ const height = svgHeight - margin.top - margin.bottom;
 // Create an SVG wrapper, append an SVG group that will hold our chart,
 // and shift the latter by left and top margins.
 const svg = d3
-  .select(".chart")
+  .select("#scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
@@ -22,3 +22,30 @@ const svg = d3
 // Append an SVG group
 const chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  // Initial Params
+  let chosenXAxis = "poverty";
+
+  // function used for updating x-scale const upon click on axis label
+  function xScale(data, chosenXAxis) {
+    // create scales
+    const xLinearScale = d3.scaleLinear()
+      .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
+        d3.max(data, d => d[chosenXAxis]) * 1.2
+      ])
+      .range([0, width]);
+
+    return xLinearScale;
+
+  }
+
+  // function used for updating xAxis const upon click on axis label
+  function renderAxes(newXScale, xAxis) {
+    const bottomAxis = d3.axisBottom(newXScale);
+
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
+
+    return xAxis;
+  }
